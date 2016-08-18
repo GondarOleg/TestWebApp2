41b1675ab737bd85ec4e3f486589890e70138f19
GondarOleg <_oleg@ua.fm>
2016-08-18 08:37:03 +0300

    First test commit

---
+++ src/test/java/Tests.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Hondar.O.M-ad on 18.08.2016.
+ */
+public class Tests {
+    @Test
+    public void checkSumFunction(){
+        assertEquals(44,Run.sum(22,22));
+    }
+
+}
